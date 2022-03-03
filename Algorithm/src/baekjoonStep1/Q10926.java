64c978a13fd53bad3d414ac821be24e639869e64
PUREUM <pureummi@gmail.com>
2022-03-03 23:54:20 +0900

    baekjoon - step1 - q1000, q1001, q1008, q10869, q10926, q18108, q10430, q2588

---
+++ Algorithm/src/baekjoonStep1/Q10926.java
@@ -0,0 +1,14 @@
+package baekjoonStep1;
+
+import java.util.Scanner;
+
+public class Q10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String id = sc.nextLine();
+		if(id.equals("joonas")) {
+			System.out.println("joonas" + "??!");
+		}
+		sc.close();
+	}
+}
