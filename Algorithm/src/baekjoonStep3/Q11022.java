745f91239d0e86a1334d8d33875a4f8f1902d134
PUREUM <pureummi@gmail.com>
2022-03-09 22:57:46 +0900

    baekjoon - step3(for) - q11022

--- Algorithm/src/baekjoonStep3/Q11022.java
+++ Algorithm/src/baekjoonStep3/Q11022.java
@@ -6,15 +6,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Q2741 {
+public class Q11022 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int a = Integer.parseInt(br.readLine());
 		
-		for(int i = 1; i < a+1; i++) {
-			bw.write(i + "\n");
+		for(int i = 0; i < a; i++) {
+			String str = br.readLine();
+			int result = str.indexOf(" ");
+			int num1 = Integer.parseInt(str.substring(0, result));
+			int num2 = Integer.parseInt(str.substring(result+1));
+			bw.write("Case #" + (i+1) + ": " + num1 + " + " + num2 + " = " + (num1+num2) + "\n");
 		}
 		
 		bw.close();
