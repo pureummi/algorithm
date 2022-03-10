4b0b0e0eabe05e59e39461e2a27ae104ffb32e79
PUREUM <pureummi@gmail.com>
2022-03-10 17:24:36 +0900

    baekjoon - step3(for) - q2438, q2439, q10871

---
+++ Algorithm/src/baekjoonStep3/Q10871.java
@@ -0,0 +1,33 @@
+package baekjoonStep3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Q10871 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer str = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(str.nextToken());
+		int x = Integer.parseInt(str.nextToken());
+
+		StringTokenizer strX = new StringTokenizer(br.readLine());
+		int[] array = new int[n];
+		for(int i = 0; i < n; i++) {
+			array[i] = Integer.parseInt(strX.nextToken());
+		}
+		
+		for(int j = 0; j < array.length; j++) {
+			if(array[j] < x) {
+				bw.write(array[j] + " ");
+			}
+		}
+		
+		bw.close();
+	}
+}
