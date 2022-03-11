f99908aae6ff16ba66b1d0843d4e9d0e7f8e5ec9
PUREUM <pureummi@gmail.com>
2022-03-12 00:30:09 +0900

    baekjoon - step4(while) - q10952, q10951, q1110

---
+++ Algorithm/src/baekjoon4/Q1110.java
@@ -0,0 +1,35 @@
+package baekjoon4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q1110 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int a = n;
+		int count = 0;
+		
+		/*
+		 * do { n = ((n % 10)*10) + (((n / 10) + (n % 10))%10); count++;
+		 * 
+		 * } while(a != n);
+		 */
+		
+		while(true) {
+			n = ((n % 10)*10) + (((n / 10) + (n % 10))%10);
+			count++;
+			
+			if(a == n) {
+				break;
+			}
+		}
+		
+		System.out.println(count);
+	
+	}
+}
