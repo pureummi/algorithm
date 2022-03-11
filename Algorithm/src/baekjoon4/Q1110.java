970bea47919d7cd8596eeffb5bacb3106ad3b679
PUREUM <pureummi@gmail.com>
2022-03-12 00:31:17 +0900

    q1110

--- Algorithm/src/baekjoon4/Q1110.java
+++ Algorithm/src/baekjoon4/Q1110.java
@@ -1,10 +1,8 @@
 package baekjoon4;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Q1110 {
 	public static void main(String[] args) throws IOException {
