cc0995db7fc1fa102660b20a3345da62fa26f912
PUREUM <pureummi@gmail.com>
2022-04-10 23:02:23 +0900

    commit test

--- Algorithm/src/baekjoonStep7/Q2292.java
+++ Algorithm/src/baekjoonStep7/Q2292.java
@@ -1,13 +1,7 @@
 package baekjoonStep7;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Q2292 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int input = Integer.parseInt(br.readLine());
-		
+	public static void main(String[] args) {
+	
 	}
 }
