90824569ae9eb771c4aa70f108c64c8da4b823e7
PUREUM <pureummi@gmail.com>
2022-04-10 22:57:19 +0900

    baekjoon - step7(basic math) - q2292

---
+++ Algorithm/src/baekjoonStep7/Q2292.java
@@ -0,0 +1,13 @@
+package baekjoonStep7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2292 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int input = Integer.parseInt(br.readLine());
+		
+	}
+}
