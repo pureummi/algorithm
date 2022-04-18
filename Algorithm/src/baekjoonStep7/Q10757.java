4f80bb4c1c8da141b470cc4b8b80bf97d633f9df
PUREUM <pureummi@gmail.com>
2022-04-18 19:41:37 +0900

    baekjoon - step7(basic math) - q10757

--- Algorithm/src/baekjoonStep7/Q10757.java
+++ Algorithm/src/baekjoonStep7/Q10757.java
@@ -3,18 +3,44 @@ package baekjoonStep7;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Q10757 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		StringTokenizer str = new StringTokenizer(br.readLine());
-		BigInteger a = new BigInteger(str.nextToken());
-		BigInteger b = new BigInteger(str.nextToken());
+		String a = str.nextToken();
+		String b = str.nextToken();
+
+		int length = Math.max(a.length(), b.length());
+
+		int[] A = new int[length+1];
+		int[] B = new int[length+1];
+
+		for (int i = a.length()-1, j = 0; i >= 0; i--, j++) {
+			A[j] = a.charAt(i) - '0';
+		}
+
+		for (int i = b.length()-1, j = 0; i >= 0; i--, j++) {
+			B[j] = b.charAt(i) - '0';
+		}
+
+		for (int k = 0; k < length; k++) {
+			int sum = A[k] + B[k];
+			A[k] = sum % 10;
+			A[k+1] += (sum / 10);
+		}
+
+		StringBuilder sb = new StringBuilder();
+		if (A[length] != 0) {
+			sb.append(A[length]);
+		} 
 		
-		System.out.println(a.add(b));
-		br.close();
+		for (int n = length-1; n >= 0; n--) {
+			sb.append(A[n]);
+		}
+
+		System.out.println(sb);
 	}
 }
