52db0cc8e0b03300b4e338ee1892e527e42a5e09
PUREUM <pureummi@gmail.com>
2022-04-25 19:37:08 +0900

    baekjoon - step8(basic math2) - q9020

--- Algorithm/src/baekjoonStep8/Q9020.java
+++ Algorithm/src/baekjoonStep8/Q9020.java
@@ -3,61 +3,44 @@ package baekjoonStep8;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Q9020 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		//4 ≤ n ≤ 10,000 이므로 n의 최댓값 +1
+		boolean[] check = new boolean[10001];
+		check[0] = check[1] = false;
+
+		for (int i = 2; i < 10001; i++) {
+			check[i] = true; 
+		}
+
+		for (int i = 2; i < 10001; i++) {
+			if (!check[i]) continue;
+			for (int j = i * i; j < 10001; j+=i) {
+				if (check[i]) check[j] = false;
+			}
+		}
+
 		int t = Integer.parseInt(br.readLine());
 		String[] result = new String[t];
-
-		List<Integer> list = new ArrayList<Integer>();
-		for (int i = 0; i < t; i++) {
-			int n = Integer.parseInt(br.readLine());
-
-			boolean[] check = new boolean[n+1];
-			check[0] = check[1] = false;
-
-			for (int j = 2; j <= n; j++) {
-				check[j] = true;
-			}
-
-			for (int j = 2; j <= n; j++) {
-				if(!check[j]) continue;
-				for (int k = 2*j; k <= n; k += j) {
-					if (check[k]) {
-						check[k] = false;
-					}
-				}
-			}
-
-			for (int j = n; j >= 0; j--) {
-				if (check[j]) {
-					list.add(j);
-				}
-			}
-
-			String output = "";
-			for (int j = 0; j < list.size(); j++) {
-				for (int k = 0; k < list.size(); k++) {
-					int gap = list.get(j) - list.get(k);
-					if ((n - list.get(j) == list.get(k))) {
-						if (gap == 0) {
-							output = list.get(j) + " " + list.get(k);
-
-						} else if (gap > 0) {
-							output = list.get(k) + " " + list.get(j);
-						}
-					} 
-				}
-			}
-
-			result[i] = output;
-		}
 		
-		for (String s: result) {
+		for (int i = 0 ; i < t; i++) {
+			int n = Integer.parseInt(br.readLine());
+			int a = n / 2;
+			int b = n / 2;
+			while (true) {
+				if (check[a] == true && check[b] == true) {
+					result[i] = a + " " + b;
+					break;
+				}
+				a--;
+				b++;
+			}
+		}
+
+		for (String s : result) {
 			System.out.println(s);
 		}
 		
