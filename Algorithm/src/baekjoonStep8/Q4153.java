07cfb9aa71ffcb639062b8f2dc7dab7c4c31cde1
PUREUM <pureummi@gmail.com>
2022-04-28 18:00:04 +0900

    baekjoon - step8(basic math2) - q4153

--- Algorithm/src/baekjoonStep8/Q4153.java
+++ Algorithm/src/baekjoonStep8/Q4153.java
@@ -3,7 +3,6 @@ package baekjoonStep8;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q4153 {
@@ -19,22 +18,19 @@ public class Q4153 {
 			int b = Integer.parseInt(str.nextToken());
 			int c = Integer.parseInt(str.nextToken());
 			
-			if (a == 0 && b == 0 && c == 0) {
-				break;
+			if (a == 0 && b == 0 && c == 0) break;
 				
+			if ((a * a) == (b * b) + (c * c)) {
+				sb.append("right" + "\n");
+			
+			} else if ((b * b) == (a * a) + (c * c)) {
+				sb.append("right" + "\n");
+			
+			} else if ((c * c) == (a * a) + (b * b)) {
+				sb.append("right" + "\n");
+			
 			} else {
-				int[] array = new int[3];
-				array[0] = a * a;
-				array[1] = b * b;
-				array[2] = c * c;
-				Arrays.sort(array);
-				
-				if (array[0] + array[1] == array[2]) {
-					sb.append("right" + "\n");
-					
-				} else {
-					sb.append("wrong" + "\n");
-				}
+				sb.append("wrong" + "\n");
 			}
 		}
 		
