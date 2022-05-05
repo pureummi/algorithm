f4851a454a18cdf790ba816202f4439f5036e5e6
PUREUM <pureummi@gmail.com>
2022-05-05 19:22:42 +0900

    baekjoon - step9(recursion) - q2447

--- Algorithm/src/baekjoonStep9/Q2447.java
+++ Algorithm/src/baekjoonStep9/Q2447.java
@@ -5,46 +5,57 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Q2447 {
+	static char[][] arr;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int input = Integer.parseInt(br.readLine());
-		recursionStar(input);
+
+		int n = Integer.parseInt(br.readLine());
+
+		arr = new char[n][n];
+		
+		recursionStar(0, 0, n, false);
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				sb.append(arr[i][j]);
+			}
+			sb.append('\n');
+		}
+
+		System.out.println(sb);
 		br.close();
 	}
 
-	public static void recursionStar(int n) {
-		if (n <= 3) {
-			for(int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					if (j >= (n/3) && j <= ((2*(n/3))-1) 
-							&& i >= (n/3) && i <= ((2*(n/3))-1)) {
-						System.out.print(" ");
-
-					}else {
-						System.out.print("*");
-
-					}
+	static void recursionStar(int x, int y, int n, boolean blank) {
+		if (blank) {
+			for (int i = x; i < x + n; i++) {
+				for (int j = y; j < y + n; j++) {
+					arr[i][j] = ' ';
 				}
-				System.out.println("");
 			}
-
-		} else if (n > 3) {
-			for(int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					if (j >= (n/3) && j <= ((2*(n/3))-1) 
-							&& i >= (n/3) && i <= ((2*(n/3))-1)) {
-						System.out.print(" ");
-
-					}else {
-						System.out.print("*");
-
-					}
+			return;
+		}
+		
+		if (n == 1) {
+			arr[x][y] = '*';
+			return;
+		}
+		
+		int size = n / 3;
+		int count = 0;
+		for (int i = x; i < x + n; i += size) {
+			for (int j = y; j < y + n; j += size) {
+				count++;
+				if (count == 5) {
+					recursionStar(i, j, size, true);
+				} else {
+					recursionStar(i, j, size, false);
 				}
-				System.out.println("");
-
 			}
 		}
 
-
 	}
+
 }
