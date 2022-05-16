27993bbada8000560d364f8e2aa083d21378e8d1
PUREUM <pureummi@gmail.com>
2022-05-16 19:40:10 +0900

    baekjoon - step11(sort) - q2750

---
+++ Algorithm/src/baekjoon/step11/Q2750.java
@@ -0,0 +1,27 @@
+package baekjoon.step11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Q2750 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] array = new int[n];
+		for (int i = 0; i < n; i++) {
+			array[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(array);
+		
+		for (int i : array) {
+			System.out.println(i);
+		}
+		
+		br.close();
+	}
+}
