b8e06b50a8a0cdb4e71408bfef457d9855a8d626
PUREUM <pureummi@gmail.com>
2022-05-26 19:36:21 +0900

    baekjoon - step11(sort) - q1427(counting sort)

--- Algorithm/src/baekjoon/step11/Q1427.java
+++ Algorithm/src/baekjoon/step11/Q1427.java
@@ -7,30 +7,37 @@ import java.io.InputStreamReader;
 public class Q1427 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		String n = br.readLine();
-		
+
 		int[] array = new int[n.length()];
+		int max = 0;
 		for (int i = 0; i < n.length(); i++) {
 			int value = Integer.parseInt(n.substring(i,i+1));
 			array[i] = value;
+
+			if (value > max) max = value;
 		}
 
-		int temp = 0;
-		for (int i = 0; i < array.length-1; i++) {
-			for (int j = 1; j < array.length-i; j++) {
-				if (array[j-1] < array[j]) {
-					temp = array[j-1];
-					array[j-1] = array[j];
-					array[j] = temp;
-				}
-			}
+		int[] counting = new int[max+1];
+		for (int i = 0; i < array.length; i++) {
+			counting[array[i]]++;
 		}
 		
-		for (int i : array) {
-			System.out.print(i);
+		for (int i = 1; i < counting.length; i++) {
+			counting[i] += counting[i-1]; 
 		}
 		
+		int[] result = new int[array.length];
+		for (int i = result.length-1; i >= 0; i--) {
+			counting[array[i]] = counting[array[i]]-1;
+			result[counting[array[i]]] = array[i];
+		}
+
+		for (int i = result.length-1; i >= 0; i--) {
+			System.out.print(result[i]);
+		}
+
 		br.close();
 	}
 }
