b2520d3143db9e9f193a496fdcb18eeefba2a709
PUREUM <pureummi@gmail.com>
2022-05-30 17:55:49 +0900

    baekjoon - step11(sort) - q11651(bubble sort)

--- Algorithm/src/baekjoon/step11/Q11651.java
+++ Algorithm/src/baekjoon/step11/Q11651.java
@@ -23,39 +23,40 @@ public class Q11651 {
 		}
 		
 		//y값을 기준으로 x값과 xy값 정렬
-		for (int i = 1; i < n; i++) {
-			int targetY = y[i];
-			int targetX = x[i];
-			String targetXY = xy[i];
-			int j = i-1;
-			
-			while (j >= 0 && y[j] > targetY) {
-				y[j+1] = y[j];
-				x[j+1] = x[j];
-				xy[j+1] = xy[j];
-				j--;
+		int tempY = 0;
+		int tempX = 0;
+		String tempXY = "0";
+		for (int i = 0; i < y.length-1; i++) {
+			for (int j = 1; j < y.length-i; j++) {
+				if (y[j-1] > y[j]) {
+					tempY = y[j-1];
+					y[j-1] = y[j];
+					y[j] = tempY;
+					
+					tempX = x[j-1];
+					x[j-1] = x[j];
+					x[j] = tempX;
+					
+					tempXY = xy[j-1];
+					xy[j-1] = xy[j];
+					xy[j] = tempXY;
+				}
 			}
-			
-			y[j+1] = targetY;
-			x[j+1] = targetX;
-			xy[j+1] = targetXY;
 		}
 		
 		//y값이 같을 때, x값을 기준으로 xy값 정렬
-		for (int i = 1; i < n; i++) {
-			int targetX = x[i];
-			String targetXY = xy[i];
-			int j = i-1;
-			
-			while (j >= 0 && x[j] > targetX
-					&& y[j] == y[j+1]) {
-				x[j+1] = x[j];
-				xy[j+1] = xy[j];
-				j--;
+		for (int i = 0; i < x.length-1; i++) {
+			for (int j = 1; j < x.length-i; j++) {
+				if (y[j-1] == y[j] && x[j-1] > x[j]) {
+					tempX = x[j-1];
+					x[j-1] = x[j];
+					x[j] = tempX;
+					
+					tempXY = xy[j-1];
+					xy[j-1] = xy[j];
+					xy[j] = tempXY;
+				}
 			}
-			
-			x[j+1] = targetX;
-			xy[j+1] = targetXY;
 		}
 		
 		for (String s : xy) {
