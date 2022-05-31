556ddab8dea6adaa909f34726e29c4fe208bcf88
PUREUM <pureummi@gmail.com>
2022-05-31 16:46:01 +0900

    baekjoon - step11(sort) - q11651 (Array.sort method, Lambda expression)

--- Algorithm/src/baekjoon/step11/Q11651.java
+++ Algorithm/src/baekjoon/step11/Q11651.java
@@ -3,6 +3,7 @@ package baekjoon.step11;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q11651 {
@@ -11,57 +12,28 @@ public class Q11651 {
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		String[] xy = new String[n];
-		int[] x = new int[n];
-		int[] y = new int[n];
+		int[][] xy = new int[n][2];
 		
 		for (int i = 0; i < n; i++) {
-			xy[i] = br.readLine();
-			StringTokenizer str = new StringTokenizer(xy[i]);
-			x[i] = Integer.parseInt(str.nextToken());
-			y[i] = Integer.parseInt(str.nextToken());
+			StringTokenizer str = new StringTokenizer(br.readLine());
+			xy[i][0] = Integer.parseInt(str.nextToken());
+			xy[i][1] = Integer.parseInt(str.nextToken());
 		}
 		
-		//y값을 기준으로 x값과 xy값 정렬
-		int tempY = 0;
-		int tempX = 0;
-		String tempXY = "0";
-		for (int i = 0; i < y.length-1; i++) {
-			for (int j = 1; j < y.length-i; j++) {
-				if (y[j-1] > y[j]) {
-					tempY = y[j-1];
-					y[j-1] = y[j];
-					y[j] = tempY;
-					
-					tempX = x[j-1];
-					x[j-1] = x[j];
-					x[j] = tempX;
-					
-					tempXY = xy[j-1];
-					xy[j-1] = xy[j];
-					xy[j] = tempXY;
-				}
+		Arrays.sort(xy, (o1, o2) -> {
+			if (o1[1] == o2[1]) {
+				return o1[0] - o2[0];
+				
+			} else {
+				return o1[1] - o2[1];
 			}
-		}
+		});
 		
-		//y값이 같을 때, x값을 기준으로 xy값 정렬
-		for (int i = 0; i < x.length-1; i++) {
-			for (int j = 1; j < x.length-i; j++) {
-				if (y[j-1] == y[j] && x[j-1] > x[j]) {
-					tempX = x[j-1];
-					x[j-1] = x[j];
-					x[j] = tempX;
-					
-					tempXY = xy[j-1];
-					xy[j-1] = xy[j];
-					xy[j] = tempXY;
-				}
-			}
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < xy.length; i++) {
+			sb.append(xy[i][0] + " " + xy[i][1]).append('\n');
 		}
-		
-		for (String s : xy) {
-			System.out.println(s);
-		}
+		System.out.println(sb);
 		
 		br.close();
 	}
