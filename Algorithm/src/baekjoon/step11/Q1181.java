d4f3d290e3b091718b997bce0a78d74e6c8b501e
PUREUM <pureummi@gmail.com>
2022-06-03 19:32:26 +0900

    baekjoon - step11(sort) - q1181(Arrays.sort, Comparator)

--- Algorithm/src/baekjoon/step11/Q1181.java
+++ Algorithm/src/baekjoon/step11/Q1181.java
@@ -3,11 +3,8 @@ package baekjoon.step11;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.Comparator;
 
 public class Q1181 {
 	public static void main(String[] args) throws IOException {
@@ -16,74 +13,34 @@ public class Q1181 {
 		int n = Integer.parseInt(br.readLine());
 
 		String[] array = new String[n];
+		
 		for (int i = 0; i < n; i++) {
 			array[i] = br.readLine();
 		}
-
-		for (int i = 1; i < n; i++) {
-			int targetL = array[i].length();
-			String target = array[i];
-			int j = i-1;
-
-			while (j >= 0 && array[j].length() > targetL) {
-				array[j+1] = array[j];
-				j--;
-			}
-			array[j+1] = target;
-		}
-		System.out.println("array : " + Arrays.toString(array));
 		
-		List<String> list = new ArrayList<String>();
-		
- 		for (String s : array) {
-			if (!list.contains(s)) list.add(s);
-		}
-
- 		String[] result = new String[list.size()];
- 		for (int i = 0; i < list.size(); i++) {
- 			result[i] = list.get(i);
- 		}
- 		System.out.println("result : " + Arrays.toString(result));
- 		
-		for (int i = 1; i < result.length; i++) {
-			int targetL = result[i].length();
-			String target = result[i];
-			int j = i-1;
-
-			int idx = 0;
-			while (j >= 0 && result[j].length() == targetL) {
-				int alphabetT = target.charAt(idx) - '0';
-				int alphabetJ = result[j].charAt(idx) - '0';
-
-				if (alphabetJ > alphabetT) {
+		Arrays.sort(array, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if (o1.length() == o2.length()) {
+					return o1.compareTo(o2);
 					
-					result[j+1] = result[j];
-					j--;
-					
-				} else if (alphabetJ == alphabetT) {
-					System.out.println("idx : " + idx + "\n"
-							+ "result[j].charAt(idx) : " + result[j].charAt(idx)
-							+ ", alphabetJ : " + alphabetJ
-							+ ", target.charAt(idx) : " + target.charAt(idx)
-							+ ", alphabetT : " + alphabetT);
-
-					idx++;
+				} else {
+					return o1.length() - o2.length();
 				} 
-
 			}
-
-			result[j+1] = target;
+		});
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(array[0]).append('\n');
+		
+		for (int i = 1; i < n; i++) {
+			if (!array[i-1].equals(array[i])) {
+				sb.append(array[i]).append('\n');
+			}
 		}
-
-		System.out.println("result : " + Arrays.toString(result));
-		//
-		//		StringBuilder sb = new StringBuilder();
-		//		for (int i = 0; i < n; i++) {
-		//			sb.append(array[i]).append('\n');
-		//		}
-		//		
-		//		System.out.println(sb);
-
+		System.out.println(sb);
+		
 		br.close();
 	}
 }
