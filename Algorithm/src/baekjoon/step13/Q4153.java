a188eb60663bb4bd80c3b335b4a1c03209468d5a
PUREUM <pureummi@gmail.com>
2022-06-15 20:02:36 +0900

    baekjoon - step13(geometry) - q4153

---
+++ Algorithm/src/baekjoon/step13/Q4153.java
@@ -0,0 +1,39 @@
+package baekjoon.step13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q4153 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while (true) {
+			StringTokenizer str = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(str.nextToken());
+			int b = Integer.parseInt(str.nextToken());
+			int c = Integer.parseInt(str.nextToken());
+			
+			if (a == 0 && b == 0 && c == 0) {
+				break;
+				
+			} else {
+				if ((a*a)+(b*b) == (c*c)) {
+					sb.append("right").append('\n');
+				
+				} else {
+					sb.append("wrong").append('\n');
+				}
+			}
+			
+		}
+		
+		System.out.println(sb);
+		
+		br.close();
+	}
+}
