fe026cc8035c1a0150183286bd0a204480623f9d
PUREUM <pureummi@gmail.com>
2022-06-22 18:32:54 +0900

    baekjoon - step13(geometry) - q1004(final)

--- Algorithm/src/baekjoon/step13/Q1004.java
+++ Algorithm/src/baekjoon/step13/Q1004.java
@@ -11,17 +11,20 @@ public class Q1004 {
 		
 		int t = Integer.parseInt(br.readLine());
 		
-		
 		int x1, y1, x2, y2;
+		
+		StringBuilder sb = new StringBuilder();
 		
 		for (int i = 0; i < t; i++) {
 			StringTokenizer str = new StringTokenizer(br.readLine());
+			
 			x1 = Integer.parseInt(str.nextToken());
 			y1 = Integer.parseInt(str.nextToken());
 			x2 = Integer.parseInt(str.nextToken());
 			y2 = Integer.parseInt(str.nextToken());
 			
 			int n = Integer.parseInt(br.readLine());
+			int cnt = 0;
 			
 			for (int j = 0; j < n; j++) {
 				str = new StringTokenizer(br.readLine());
@@ -30,15 +33,22 @@ public class Q1004 {
 				int c2 = Integer.parseInt(str.nextToken());
 				int r = Integer.parseInt(str.nextToken());
 				
-				System.out.println("c1 : " + c1);
-				System.out.println("c2 : " + c2);
-				System.out.println("r : " + r);
+				int distance1 = (int)Math.pow(c1-x1, 2) + (int)Math.pow(c2-y1, 2);
+				int distance2 = (int)Math.pow(c1-x2, 2) + (int)Math.pow(c2-y2, 2);
+				
+				//하나의 원 안에 출발점과 도착점이 모두 존재하는 경우는 횟수에 포함되면 X
+				if (!(distance1 < Math.pow(r, 2) && distance2 < Math.pow(r, 2)) 
+						&& (distance1 < Math.pow(r, 2) || distance2 < Math.pow(r, 2))) {
+					
+					cnt++;
+				}
 			}
 			
-			System.out.println("x1 : " + x1);
-			System.out.println("y1 : " + y1);
-			System.out.println("x2 : " + x2);
-			System.out.println("y2 : " + y2);
+			sb.append(cnt).append('\n');
 		}
+		
+		System.out.println(sb.toString().trim());
+		
+		br.close();
 	}
 }
