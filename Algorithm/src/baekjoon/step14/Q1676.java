54e7b70ee448d3f675795242fbbcacd0c59b3052
PUREUM <pureummi@gmail.com>
2022-07-21 19:06:12 +0900

    baekjoon - step14(number theory and combinatorics) - q1676(final)

--- Algorithm/src/baekjoon/step14/Q1676.java
+++ Algorithm/src/baekjoon/step14/Q1676.java
@@ -3,7 +3,6 @@ package baekjoon.step14;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class Q1676 {
 	public static void main(String[] args) throws IOException {
@@ -11,25 +10,11 @@ public class Q1676 {
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		BigInteger result = BigInteger.valueOf(1);
-		
-		while (n >= 2) {
-			result = result.multiply(BigInteger.valueOf(n));
-			n--;
-		}
-		
-		String resultStr = String.valueOf(result);
-		
 		int cnt = 0;
 		
-		for (int i = resultStr.length(); i >= 1; i--) {
-			if (resultStr.substring(i-1, i).equals("0")) {
-				cnt++;
-			}
-			
-			if (cnt >= 1 && !resultStr.substring(i-1, i).equals("0")) {
-				break;
-			}
+		while (n >= 5) {
+			cnt += n/5;
+			n = n/5;
 		}
 		
 		System.out.println(cnt);
