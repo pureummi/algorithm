4942435ae268479ad16ad1d137a6d0a624639af6
PUREUM <pureummi@gmail.com>
2022-07-26 17:26:20 +0900

    baekjoon - step15(backtracking) - q15651(final)

--- Algorithm/src/baekjoon/step15/Q15651.java
+++ Algorithm/src/baekjoon/step15/Q15651.java
@@ -7,26 +7,24 @@ import java.util.StringTokenizer;
 
 public class Q15651 {
 	static int[] arr;
-	static boolean[] check;
 	static int n, m;
 	static StringBuilder sb = new StringBuilder();
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		StringTokenizer str = new StringTokenizer(br.readLine());
-		
+
 		n = Integer.parseInt(str.nextToken());
 		m = Integer.parseInt(str.nextToken());
-		
+
 		arr = new int[m];
-		check = new boolean[n];
-		
+
 		backtracking(0);
-		
+
 		System.out.println(sb);
 	}
-	
+
 	static void backtracking(int idx) {
 		if (idx == m) {
 			for (int i : arr) {
@@ -34,14 +32,11 @@ public class Q15651 {
 			}
 			sb.append('\n');
 			return;
-			
+
 		} else {
 			for (int i = 0; i < n; i++) {
-				if (!check[i]) {
-					arr[idx] = i+1;
-					backtracking(idx+1);
-					check[i] = false;
-				}
+				arr[idx] = i+1;
+				backtracking(idx+1);
 			}
 		}
 	}
